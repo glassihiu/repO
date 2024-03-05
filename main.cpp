d098124d433a2a016374ee36717c02cbf30ba8a7
Jonathan Darlene <162285459+glassihiu@users.noreply.github.com>
2024-03-05 09:11:29 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+if (!line.compare(0, 2, "v ")) {
+            iss >> trash;
+            vec3 v;
+            for (int i=0;i<3;i++) iss >> v[i];
+            verts.push_back(v);
+        } else if (!line.compare(0, 3, "vn ")) {
+            iss >> trash >> trash;
+            vec3 n;
+            for (int i=0;i<3;i++) iss >> n[i];
+            norms.push_back(n.normalized());
+        } else if (!line.compare(0, 3, "vt ")) {
+            iss >> trash >> trash;
+            vec2 uv;
+            for (int i=0;i<2;i++) iss >> uv[i];
+            tex_coord.push_back({uv.x, 1-uv.y});
+        }  else if (!line.compare(0, 2, "f ")) {
+            int f,t,n;
+            iss >> trash;
+            int cnt = 0;
